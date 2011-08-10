12ac36a7d9add27023fe7f0d9d8ddc54f1e1f662
Luis Godinez <luisgodinez@gmail.com>
2011-08-10 10:08:11 -0500

    update for mac

--- colors.sh
+++ colors.sh
@@ -2,5 +2,5 @@
 # off by default to not distract the user: the focus in a terminal window
 # should be on the output of commands, not on the prompt
 
-alias ls='ls --color=auto'
+#alias ls='ls --color=auto'
 
--- config.sh
+++ config.sh
@@ -2,6 +2,7 @@ export PS1='\[\033[1;34m\]\W\[\033[1;31m\]$(__git_ps1 " [%s]") \[\033[1;32m\]\$ 
 export EDITOR='vim'
 export SVN_EDITOR='vim'
 
-
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
 
+PATH=$PATH:/opt/local/bin:/opt/local/sbin
+export PATH
